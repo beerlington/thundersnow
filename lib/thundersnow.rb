33ff847b32fae2dfc56845f2fffb2b592e986a83
beerlington <pete@lette.us>
2011-02-07 23:03:36 -0500

    Added basic cli functionality w/specs

---
+++ lib/thundersnow.rb
@@ -0,0 +1,72 @@
+require 'nokogiri'
+require 'open-uri'
+
+class Thundersnow
+  class << self
+    def run(location)
+      @xml = Nokogiri::XML(open("http://www.google.com/ig/api?weather=#{location}"))
+
+      show :city
+      puts "-------------------------"
+      show :condition
+      show :temperatures
+      show :wind
+      show :humidity
+      puts "-------------------------"
+      show :forecast
+    end
+
+  private
+
+    def show(name)
+      puts send(name)
+    end
+
+    def city
+      city = read_attr(@xml, '//forecast_information/city')
+      "Weather forecast for #{city}"
+    end
+
+    def condition
+      read_attr(current_conditions, 'condition')
+    end
+
+    def temperatures
+      "#{temp_f} / #{temp_c}"
+    end
+
+    def wind
+      read_attr(current_conditions, 'wind_condition')
+    end
+
+    def humidity
+      read_attr(current_conditions, 'humidity')
+    end
+
+    def temp_f
+      read_attr(current_conditions, 'temp_f').to_s + 'F'
+    end
+
+    def temp_c
+      read_attr(current_conditions, 'temp_c').to_s + 'C'
+    end
+
+    def current_conditions
+      @current_conditions ||= @xml.xpath('//current_conditions')
+    end
+
+    def forecast
+      @xml.xpath('//forecast_conditions').map do |day|
+        "Forecast for #{read_attr(day, 'day_of_week')} \n" +
+        "High: #{read_attr(day, 'high')}F / Low: #{read_attr(day, 'low')}F \n" +
+        "Conditions: #{read_attr(day, 'condition')} \n" +
+        "-------------------------\n"
+      end
+    end
+
+    def read_attr(root, node)
+      root.xpath(node).attribute('data')
+    end
+  end
+
+end
--- spec/thundersnow_spec.rb
+++ spec/thundersnow_spec.rb
@@ -1,7 +1,25 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Thundersnow" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  before do
+    Thundersnow.stub(:open).and_return(XML)
+  end
+
+  context "when running" do
+    before do
+      Thundersnow.stub(:show).and_return(true)
+    end
+
+    it "should set the @xml instance variable" do
+      Thundersnow.run ZIP
+      Thundersnow.instance_variable_get('@xml').to_s.should == Nokogiri::XML(XML).to_s
+    end
+
+    [:city, :condition, :temperatures, :wind, :humidity, :forecast].each do |node|
+      it "should show the #{node}" do
+        Thundersnow.should_receive(:show).with(node).once
+        Thundersnow.run ZIP
+      end
+    end
   end
 end
